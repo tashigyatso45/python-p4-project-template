0444e29d4f226217ef49d65be32b4384fbf9f174
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-12-06 17:36:31 -0500

    Established many to many relationship between user, answer, and question card

--- server/models.py
+++ server/models.py
@@ -13,6 +13,8 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String)
     grade_level = db.Column(db.Integer)
     card_decks = db.relationship('Card_Deck', backref= 'users')
+    answers = db.relationship('Answer', back_populates = 'user', cascade='all, delete-orphan')
+    question_cards = association_proxy('answers', 'question_card')
     
     @property
     def password_hash(self):
@@ -44,6 +46,11 @@ class Answer(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     user_answer = db.Column(db.String)
     is_correct = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question_card_id = db.Column(db.Integer, db.ForeignKey('question_cards.id'))
+    user = db.relationship('User', back_populates = 'answers')
+    question_card = db.relationship('Question_Card', back_populates = 'answers')
+
     
 
 class Card_Deck(db.Model, SerializerMixin):
@@ -51,7 +58,8 @@ class Card_Deck(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    question_cards = db.relationship('Question_Card', backref= 'card_decks')
+    question_card_id = db.Column(db.Integer, db.ForeignKey('question_cards.id'))
+    question_card = db.relationship('Question_Card', backref= 'card_decks')
 
 
 class Question_Card(db.Model, SerializerMixin):
@@ -60,6 +68,8 @@ class Question_Card(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     is_complete = db.Column(db.Boolean)
     card_deck_id = db.Column(db.Integer,db.ForeignKey('card_decks.id'))
+    answers = db.relationship('Answer', back_populates = 'question_card', cascade='all, delete-orphan')
+    users = association_proxy('answers', 'user')
 
 
 
